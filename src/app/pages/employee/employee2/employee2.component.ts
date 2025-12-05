2421f89e9eee69cb84c4864eaec7d554b8ffad5e
sara <saramoamen2004@gmail.com>
2025-12-05 17:14:06 +0200

    some changes

--- src/app/pages/employee/employee2/employee2.component.ts
+++ src/app/pages/employee/employee2/employee2.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeaderComponent } from '../../../components/header/header.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
+
 
 @Component({
   selector: 'app-employee2',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './employee2.component.html',
   styleUrl: './employee2.component.css'
 })
