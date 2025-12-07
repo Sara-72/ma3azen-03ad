74948316c7027f390ae4024c0fad714c810449e8
sara <saramoamen2004@gmail.com>
2025-12-07 20:55:45 +0200

    admin added

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidationErrors,
+  AbstractControl,
+  FormBuilder,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent ,CommonModule,
+    ReactiveFormsModule,
+
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
