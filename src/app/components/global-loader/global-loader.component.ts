fe7e38cfdb2815b478ad432295b7982d1d02f8eb
sara <saramoamen2004@gmail.com>
2026-01-15 09:11:08 +0200

    loader

--- src/app/components/global-loader/global-loader.component.ts
+++ src/app/components/global-loader/global-loader.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { LoadingService } from '../../services/loading.service';
+import { Component, inject } from '@angular/core';
 
+import { Router, NavigationEnd } from '@angular/router';
+import { map, startWith, combineLatest, filter } from 'rxjs'; // <--- Added 'filter' here
 @Component({
   selector: 'app-global-loader',
   standalone: true,
@@ -10,5 +13,25 @@ import { LoadingService } from '../../services/loading.service';
   styleUrl: './global-loader.component.css'
 })
 export class GlobalLoaderComponent {
-  constructor(public loadingService: LoadingService) {}
+private router = inject(Router);
+  private loadingService = inject(LoadingService);
+
+  // List all URL segments where you don't want the loader to appear
+  private excludedRoutes = ['/login', '/login2', '/login3', '/login4','/login5'];
+
+  showLoader$ = combineLatest([
+    this.loadingService.isLoading$,
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+      startWith(null),
+      map(() => this.router.url)
+    )
+  ]).pipe(
+    map(([isLoading, url]) => {
+      // .some() checks if at least one excluded route is present in the current URL
+      const isExcluded = this.excludedRoutes.some(route => url?.includes(route));
+
+      return isLoading && !isExcluded;
+    })
+  );
 }
